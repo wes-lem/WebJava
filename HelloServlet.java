9361c44a50dc92bd175c21709334db460a00f5a7
Weslem <91810375+wes-lem@users.noreply.github.com>
2025-06-18 20:04:25 -0300

    Add files via upload

---
+++ HelloServlet.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Olá Mundo!</h1>");
+    }
+}
